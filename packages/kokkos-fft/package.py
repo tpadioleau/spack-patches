0b02f28d6d9feb67a994396ee264f9495837acb4
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-02-26 07:35:36 +0100

    Update Kokkos-fft recipe

--- packages/kokkos-fft/package.py
+++ packages/kokkos-fft/package.py
@@ -12,7 +12,7 @@ class KokkosFft(CMakePackage):
     homepage = "https://github.com/kokkos/kokkos-fft"
     url = "https://github.com/kokkos/kokkos-fft/archive/refs/tags/v0.2.1.tar.gz"
 
-    maintainers("cedricchevalier19")
+    maintainers("cedricchevalier19", "tpadioleau", "yasahi-hpc")
 
     license("Apache-2.0 WITH LLVM-exception OR MIT", checked_by="cedricchevalier19")
 
@@ -22,23 +22,39 @@ class KokkosFft(CMakePackage):
 
     depends_on("cxx", type="build")
 
-    depends_on("kokkos")
+    depends_on("kokkos@4.4.00: +complex_align")
 
     variant("host", default=False, description="Enable host fft, i.e. fftw")
+    variant("unit_test", default=False, description="Enable Unit Tests")
 
-    depends_on("googletest")
     depends_on("cuda", when="^kokkos +cuda")
+
+    # FIXME: deal with rocfft
     depends_on("hipfft", when="^kokkos +rocm")
 
-    depends_on("fftw~mpi", when="^kokkos ~cuda ~rocm ~sycl")
-    depends_on("fftw~mpi", when="+host")
+    # FIXME: take the correct configuration for fftw:
+    #   - +openmp if ^kokkos+openmp ~gpu || +host ^kokkos+openmp
+    #   - ~openmp if ^kokkos+openmp ~gpu || +host ^kokkos+openmp
+    #   - "+threads": not available as a variant in spack
+    depends_on("fftw~mpi precision=float,double", when="^kokkos ~cuda ~rocm ~sycl")
+    depends_on("fftw +openmp", when="^kokkos +openmp")
+    depends_on("fftw~mpi precision=float,double", when="+host")
 
     depends_on("intel-oneapi-mkl", when="^kokkos +sycl")
+
+    # FIXME: how to handle other kokkos backends: hpx, openmp target, openacc
+
+    depends_on("googletest", when="+unit_test")
 
     def cmake_args(self):
         args = [
             self.define("KokkosFFT_ENABLE_INTERNAL_KOKKOS", False),
             self.define_from_variant("KokkosFFT_ENABLE_HOST_AND_DEVICE", "host"),
-            self.define("KokkosFFT_ENABLE_TESTS", True),
+            self.define("KokkosFFT_ENABLE_TESTS", "unit_test"),
+            self.define("KokkosFFT_ENABLE_DOCS", False),
+            self.define("KokkosFFT_ENABLE_BENCHMARK", False),
+            self.define("KokkosFFT_ENABLE_EXAMPLES", False),
+            self.define("CMAKE_CXX_COMPILER", self["kokkos"].kokkos_cxx),
         ]
+
         return args
